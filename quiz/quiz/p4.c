21fd6f37cbbfba7ee7044025fca198b2ff7a5650
Pedrest15 <pedroandrade@usp.br>
2024-07-07 22:50:38 -0300

    quiz feito

---
+++ quiz/quiz/p4.c
@@ -0,0 +1,19 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 Pedrest15 <pedroandrade@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Pedrest15 <pedroandrade@usp.br>
+ */
+
+void foo();
+void bar();
+
+int main()
+{
+  foo();
+  return 0;
+}
---
+++ quiz/quiz/p4a.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 Pedrest15 <pedroandrade@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Pedrest15 <pedroandrade@usp.br>
+ */
+
+#include <stdio.h>
+
+void foo()
+{
+  printf ("Foo\n");
+}
